0c6bad52996af0de17480231c62ef623f56bf088
Jadson Santos <jadson.santos@ufrn.br>
2021-11-04 17:49:25 -0300

    setup log on spring boot
    Signed-off-by: Jadson Santos <jadson.santos@ufrn.br>

---
+++ monitoring-demo/src/main/java/br/com/example/jadson/monitoringdemo/controllers/LoggingController.java
@@ -0,0 +1,51 @@
+package br.com.example.jadson.monitoringdemo.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A controller that generate logs.
+ * Spring Boot Starter uses ***Logback*** by default.
+ */
+@RestController
+public class LoggingController {
+
+    /**
+     * The Simple Logging Facade for Java (SLF4J) serves as a simple facade or abstraction for various logging
+     * frameworks (e.g. java.util.logging, logback, log4j) allowing the end user to plug in the desired logging
+     * framework at deployment time.
+     */
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @GetMapping("/index")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+    @GetMapping("/tracing")
+    public String tracing() {
+        try{
+            throw new NullPointerException("NullPointerException");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            StackTraceElement[] elements = ex.getStackTrace();
+            StackTraceElement[] newElements = new StackTraceElement[5];
+            int limit = elements.length > 5 ? 5 : elements.length;
+            for (int i = 0; i < limit ; i++)
+                newElements[i] = elements[i];
+            ex.setStackTrace(newElements);
+            logger.error("ERROR: "+ex.getLocalizedMessage(), ex);
+        }
+
+        return "Howdy! Check out the Logs to see the error...";
+    }
+}
