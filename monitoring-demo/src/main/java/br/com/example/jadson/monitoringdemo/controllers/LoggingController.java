a6f154911b4b303a5b1a0147ef3ec29f01defbee
Jadson Santos <jadson.santos@ufrn.br>
2021-11-05 12:33:17 -0300

    logback configuration
    Signed-off-by: Jadson Santos <jadson.santos@ufrn.br>

--- monitoring-demo/src/main/java/br/com/example/jadson/monitoringdemo/controllers/LoggingController.java
+++ monitoring-demo/src/main/java/br/com/example/jadson/monitoringdemo/controllers/LoggingController.java
@@ -1,5 +1,6 @@
 package br.com.example.jadson.monitoringdemo.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Spring Boot Starter uses ***Logback*** by default.
  */
 @RestController
+@Slf4j
 public class LoggingController {
 
     /**
@@ -22,11 +24,16 @@ public class LoggingController {
 
     @GetMapping("/index")
     public String index() {
-        logger.trace("A TRACE Message");
-        logger.debug("A DEBUG Message");
+        // logger.trace("A TRACE Message");
+        // logger.debug("A DEBUG Message");
         logger.info("An INFO Message");
         logger.warn("A WARN Message");
         logger.error("An ERROR Message");
+
+        log.info("An INFO Message Using Lombok  !!! ");
+        log.error("An ERROR Message Using Lombok !!! ");
+
+        System.out.println("This is not a log !!!");
 
         return "Howdy! Check out the Logs to see the output...";
     }
